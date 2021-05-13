cb67bf3d6da11b3cdace20605704723f4917af45
owenmoogk <owenmoogk@gmail.com>
2021-05-13 14:51:36 -0400

    reorganized file structure, added lists for showcase

---
+++ js/buttons.js
@@ -0,0 +1,40 @@
+cadButton = document.getElementById('cadButton')
+codeButton = document.getElementById('codeButton')
+showcaseButton = document.getElementById('showcaseButton')
+
+function showCad(){
+	clearButtons()
+	clearContent()
+	cadButton.style.backgroundColor = 'rgb(153,153,25)'
+	document.getElementById('stlViewer').style.display = 'block'
+	loadSTL()
+}
+
+function showCode(){
+	clearButtons()
+	clearContent()
+	document.getElementById('codeList').style.display = 'flex'
+	codeButton.style.backgroundColor = 'rgb(153,153,25)'
+}
+
+function showVideo(){
+	clearButtons()
+	clearContent()
+	showcaseButton.style.backgroundColor = 'rgb(153,153,25)'
+}
+
+function closeCad(){
+	clearButtons()
+	document.getElementById('stlViewer').style.display = 'none'
+}
+
+function clearContent(){
+	document.getElementById('codeList').style.display = 'none'
+	document.getElementById('cadList').style.display = 'none'
+}
+
+function clearButtons(){
+	codeButton.style.backgroundColor = 'rgb(50,50,50)'
+	cadButton.style.backgroundColor = 'rgb(50,50,50)'
+	showcaseButton.style.backgroundColor = 'rgb(50,50,50)'
+}
